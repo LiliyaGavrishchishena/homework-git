b28c0fcc9dca393a6549df75710aad4e7a6839a0
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-06-12 10:02:17 +0300

    feature/answers

--- js/answers.js
+++ js/answers.js
@@ -1,16 +1,17 @@
 function saveAnswer(question, answerText) {
   const { correctOption } = question;
 
-  const isCorrect = correctOption === null ? null : correctOption === answerText;
-  return { question, answerText, isCorrect }
+  const isCorrect =
+    correctOption === null ? null : correctOption === answerText;
+  return { question, answerText, isCorrect };
 }
 
 function askQuestion(question) {
   let text = question.text;
 
-  if(question.options) {
+  if (question.options) {
     text += '\n';
-    text = question.options.map(opt => '- ' + opt).join('\n')
+    text += question.options.map(opt => '- ' + opt).join('\n');
   }
 
   const answerText = prompt(text);
